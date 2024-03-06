c8836d724d607fe55f8b1e723f78e4bbef042a70
bnkc <levostatnigrosh@gmail.com>
2024-03-05 21:35:26 -0700

    quick push

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
+use ast::get_deps;
 use clap::Parser;
 use std::path::PathBuf;
-
-use ast::get_deps;
 
 mod analyze;
 mod ast;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(version, about, long_about = None)]
 pub struct Arguments {
     #[arg(default_value = ".")]
     pub path: PathBuf,
 }
 
-fn main() {
+fn run() -> Result<(), Box<dyn std::error::Error>> {
     let args = Arguments::parse();
     let deps = get_deps(&args.path);
-    println!("{:?}", deps)
+    println!("{:?}", deps);
+    Ok(())
+}
+
+fn main() {
+    let result = run();
+    match result {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("[pip-udeps erro]: {:#}", err);
+            // at some point code back and make these error code more meaningful
+            std::process::exit(1);
+        }
+    }
 }
