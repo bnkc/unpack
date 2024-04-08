841d47063fea8af02b8f07c41564d3ecb877252c
bnkc <levostatnigrosh@gmail.com>
2024-04-07 19:45:01 -0600

    quick push

--- src/project_assets/mod.rs
+++ src/project_assets/mod.rs
@@ -1,9 +1,9 @@
-mod dependencies;
-mod imports;
-mod packages;
+mod dependency;
+mod import;
+mod package;
 
 #[allow(unused_imports)]
-pub(crate) use dependencies::{get_dependencies, Dependency, DependencyBuilder};
-pub(crate) use imports::get_imports;
+pub(crate) use dependency::{get_dependencies, Dependency, DependencyBuilder};
+pub(crate) use import::get_imports;
 #[allow(unused_imports)]
-pub(crate) use packages::{get_packages, get_site_packages, Package, PackageBuilder, PackageState};
+pub(crate) use package::{get_packages, get_site_packages, Package, PackageBuilder, PackageState};
